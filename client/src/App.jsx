87dad8cc1da5d27e3b6c8b024473b847120d46f7
david-rai <davidkhaling123@gmail.com>
2025-04-14 05:02:32 -0700

    working on the client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,49 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const App = () => {
-const [res,setRes]=useState(null)
+  const [site, setSite] = useState([])
 
-useEffect(()=>{
+  //fetching the data from the servers
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch('http://localhost:1111/', {
+        method: "get",
+        credentials: 'include'
+      })
+      const data = await res.json()
+      setSite(data)
+    }
 
-async function getData(){
-const res=await fetch('http://localhost:1111')
-const data=await res.json()
-console.log(data)
-setRes(data)
-}
+    getData()
 
+    return () => setSite([])
+  }, [])
 
-return getData
-},[])
 
   return (
     <>
-    <main>
-      
-    </main>
+      <main className="h-screen bg-slate-50 w-full flex px-[20px] flex-col items-center justify-center">
+        <h1 className="p-4 text-4xl">Favourite site</h1>
+        <section className=" flex flex-wrap items-center justify-between">
+          {
+            site ? site.map((s, index) => {
+              return (
+                <div className="relative hover:bg-slate-200 p-4 transition w-[24%]" key={index} >
+                  <BsThreeDotsVertical className="absolute right-0 "/>
+                  <div className="m-3 bg-gray-200 rounded-full p-[20px]" >
+                    <a href={s.site_path} target="_blank">
+                      <img src={s.img_path} alt="" />
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+              :
+              <h1>no favourite pages</h1>
+          }
+        </section>
+      </main>
     </>
   )
 }
