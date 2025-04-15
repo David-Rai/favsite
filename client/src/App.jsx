b9b8448b952d1b0251fbe94ae900e5289ebdab5c
david-rai <davidkhaling123@gmail.com>
2025-04-15 16:46:21 +0545

    updating site feature implementing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react"
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdModeEditOutline } from "react-icons/md";
 import { IoMdAdd } from "react-icons/io";
 
 const App = () => {
@@ -8,18 +9,19 @@ const App = () => {
   const formRef = useRef(null)
   const [site_path, setName] = useState("")
   const inputRef = useRef(null)
+  const [updating,setUpdating]=useState(false)
+  const [id,setID]=useState(null)
+  async function getData() {
+    const res = await fetch('http://localhost:1111/', {
+      method: "get",
+      credentials: 'include'
+    })
+    const data = await res.json()
+    setSite(data)
+  }
 
   //fetching the data from the servers
   useEffect(() => {
-    async function getData() {
-      const res = await fetch('http://localhost:1111/', {
-        method: "get",
-        credentials: 'include'
-      })
-      const data = await res.json()
-      setSite(data)
-    }
-
     getData()
 
     return () => setSite([])
@@ -42,12 +44,11 @@ const App = () => {
   //editing the site
   const handleDelete = async (index) => {
     try {
-      alert(index)
       const res = await fetch(`http://localhost:1111/delete/${index}`, {
         method: "DELETE"
       })
       const results = await res.json()
-      console.log(results)
+      getData()
     } catch (err) {
       console.log(err)
     }
@@ -65,6 +66,7 @@ const App = () => {
 
       })
       inputRef.current.value = ""
+      getData()
 
     } catch (err) {
       console.log(err)
@@ -72,29 +74,65 @@ const App = () => {
 
   }
 
+  //toggle updating the sites path
+  const handleUpdate = async (index) => {
+    console.log("update toggle",index)
+    handleAdd()
+    setUpdating(!updating)
+    setID(index)
+  }
+
+  const handleUpdating=async ()=>{
+    console.log("updating")
+setName(inputRef.current.value)
+console.log(inputRef.current.value)
+
+    try {
+      const res = await fetch(`http://localhost:1111/update`, {
+        method: "PUT",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({site_path,id})
+      })
+      const results=await res.json()
+      console.log(results)
+    } catch (err) {
+      console.log(err)
+    }
+
+    handleAdd()
+    setUpdating(!updating)
+  }
 
   return (
     <>
       <main className="h-screen bg-slate-50 w-full flex px-[20px] flex-col items-center justify-center">
         <h1 className="p-4 text-4xl">Favourite site</h1>
         <section className=" flex flex-wrap items-center ">
-          <form
+          <div
             ref={formRef}
-            onSubmit={handleSubmit}
-            className="absolute bg-black/40 h-[160px]
-               hidden translate z-50 rounded-md
+            className="cover absolute h-full 
+            hidden w-full top-0 left-0  items-center justify-center">
+            <form
+              onSubmit={(e)=>{
+                e.preventDefault()
+                updating ? handleUpdating() : handleSubmit(e) 
+              }}
+              className="absolute bg-black/40 h-[160px]
+             translate z-50 rounded-md lg:w-[40%] flex
              w-[80%] backdrop-blur-[20px]  items-center justify-center">
-            <input type="text" ref={inputRef} name="inputdata" className="h-[40px] pl-3 rounded-md" placeholder="enter the site path" />
-            <button type="submit" className="h-[40px] w-[120px] rounded-md  text-white m-2 bg-blue-600">ADD</button>
-          </form>
+              <input type="text" ref={inputRef} name="inputdata" className="h-[40px] pl-3 rounded-md" placeholder="enter the site path" />
+              <button type="submit" className="h-[40px] w-[120px] rounded-md  text-white m-2 bg-blue-600">{updating ? "UPDATE" : "ADD"}</button>
+            </form>
+          </div>
+
           {
-            site ? site.map((s, index) => {
+            site.length > 0 ? site.map((s, index) => {
               return (
                 <div className="group relative hover:bg-slate-200 transition flex items-center justify-center h-[100px] w-[100px]" key={index} >
-                  <RiDeleteBin6Line
-                    onClick={() => handleDelete(s.id)}
-                    className="group-hover:block text-red-500
-                  hidden absolute m-1 right-0 top-0" />
+                  <RiDeleteBin6Line onClick={() => handleDelete(s.id)} className="group-hover:block text-red-500 hidden absolute m-1 right-0 top-0" />
+                  <MdModeEditOutline onClick={() => handleUpdate(s.id)} className="group-hover:block  hidden absolute m-1 right-0 bottom-0" />
                   <div className="m-3 bg-gray-300 rounded-full flex items-center justify-center h-1/2 w-1/2" >
                     <a href={s.site_path} target="_blank">
                       <img src={s.img_path} className="" />
