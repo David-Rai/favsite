e7c86c53e40fa314948bf138597384d79be404aa
David <166421323+David-Rai@users.noreply.github.com>
2025-04-20 20:13:56 +0545

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Signup from './components/signup.jsx'
+import Signup from './components/Signup.jsx'
 
 createRoot(document.getElementById('root')).render(
 <>
